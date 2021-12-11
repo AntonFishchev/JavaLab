3ef505924040a2d22af70cc01f387219a6c8bede
Anton Fishchev <antonfishevv@gmail.com>
2021-12-11 22:46:23 +0300

    lab10

---
+++ src/main/java/lab9/Main.java
@@ -0,0 +1,32 @@
+package lab9;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main {
+
+    static final String DB_URL = "jdbc:postgresql://[::1]:5432/java";
+    static final String USER = "postgres";
+    static final String PASS = "1234";
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            if (!connection.isClosed()){
+                System.out.println("Соединение с бд установлено");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
